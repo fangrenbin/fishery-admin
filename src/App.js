7fab226418361e17b41767fa12bc0db7862b7939
fangrenbin <fangrenbin@gmail.com>
2017-02-16 17:48:03 +1300

    added resources

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SpeciesTable from './components/speciesTable'
+// import ImageForm from './components/imageForm'
 import './App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="container">
         <SpeciesTable />
+        {/* <ImageForm /> */}
       </div>
     );
   }
---
+++ src/components/imageForm.js
@@ -0,0 +1,18 @@
+// import React, { Component } from 'react';
+// import { cloudinaryConfig, CloudinaryImage } from 'react-cloudinary';
+//
+// class ImageForm extends Component {
+//   render() {
+//
+//   }
+//
+//   // render(){
+//     // return (
+// // cloudinaryConfig({ cloud_name: 'otagopoly' });
+//       // const imagePublicId = 'XQx1GbeXKsHiIo183Pa-KdUIpCI';
+//       // <CloudinaryImage publicId={imagePublicId} options={{ width: 300, height: 300 }} />
+//     // )
+//   // };
+// }
+//
+// export default ImageForm;
